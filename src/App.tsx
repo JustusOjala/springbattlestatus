b5880022312fd0716d4533cd4f59a16f7f24a483
Justus Ojala <justus.ojala@aalto.fi>
2025-04-04 13:19:57 +0300

    Add sport info

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,39 @@ import './App.css'
 
 const backend: String = import.meta.env.VITE_BACKEND_URL
 
+type Guild = "SIK" | "KIK";
+
+enum Sport {
+  activity = "Activity",
+  biking = "Biking",
+  running_walking = "Running/Walking",
+}
+
+interface SportInfo {
+  sport: Sport;
+  sik_sum: Number;
+  kik_sum: Number;
+  sik_entries: Number;
+  kik_entries: Number;
+}
+
+function SportRow(sport: SportInfo){
+  return (
+    <tr>
+      <td>{sport.sport.toString()}</td>
+      <td>{sport.sik_entries.toString()}</td>
+      <td>{sport.sik_sum.toString()}</td>
+      <td>{sport.kik_sum.toString()}</td>
+      <td>{sport.kik_entries.toString()}</td>
+    </tr>
+  )
+}
+
 function App() {
-  axios.get(backend.concat("/sports")).then((/*response*/) => {
-    //const sports = response.data;
+  var sports: SportInfo[] = [];
+
+  axios.get(backend.concat("/sports")).then((response) => {
+    sports = response.data;
   });
 
   return (
@@ -24,7 +54,18 @@ function App() {
       <h1>SIK vs KIK spring battle 2025</h1>
       <div className="card">
         <table>
-          
+          <tbody>
+            <tr>
+              <td>Sport</td>
+              <td>SIK entries</td>
+              <td>SIK distance</td>
+              <td>KIK distance</td>
+              <td>KIK entries</td>
+            </tr>
+            {SportRow(sports[0])}
+            {SportRow(sports[1])}
+            {SportRow(sports[2])}
+          </tbody>
         </table>
       </div>
     </>
