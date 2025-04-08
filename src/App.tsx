94d4f85859244b09d58dc5a1574f17da9f97a1e2
Justus Ojala <justus.ojala@aalto.fi>
2025-04-08 22:24:12 +0300

    Implement "healthbars"

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,20 @@ function SportRow(sport: SportInfo){
   const difference: number = Math.abs(sport.sik_sum - sport.kik_sum);
   const leaderClass: string = leader ? leader === "SIK" ? "sik" : "kik" : ""
 
+  const numSymbols = 25;
+  const sikSymbols = leader ? Math.round(numSymbols*(sport.sik_sum/(sport.sik_sum + sport.kik_sum))) : Math.floor(numSymbols / 2);
+  const kikSymbols = leader ? numSymbols - sikSymbols : Math.floor(numSymbols / 2);
+
+  const leftSikSymbols = "\u26A1".repeat(Math.min(sikSymbols, Math.floor(numSymbols / 2)));
+  const leftKikSymbols = "\u2699".repeat(Math.max(Math.floor(numSymbols / 2) - sikSymbols, 0))
+  const middleSymbol = leader ? leader === "SIK" ? "\u26A1" : "\u2699" : "=";
+  const rightSikSymbols = "\u26A1".repeat(Math.max(Math.floor(numSymbols / 2) - kikSymbols, 0));
+  const rightKikSymbols = "\u2699".repeat(Math.min(kikSymbols, Math.floor(numSymbols / 2)));
+
   return (
     <>
       <tr>
-        <td></td><td></td>
-        <td className={`sportHeader ${leaderClass}`}>{sport.sport}</td>
-        <td></td><td></td>
+        <td colSpan={5} className={`sportHeader ${leaderClass}`}>{sport.sport}</td>
       </tr>
       <tr>
         <td className="sik">{sport.sik_entries.toString()}</td>
@@ -40,6 +48,9 @@ function SportRow(sport: SportInfo){
         <td className={leaderClass}>{leader ? `${leader} by ${difference.toFixed(1)}` : "even"}</td>
         <td className="kik">{sport.kik_sum.toFixed(1)}</td>
         <td className="kik">{sport.kik_entries.toString()}</td>
+      </tr>
+      <tr>
+        <td colSpan={5}>{leftSikSymbols}{leftKikSymbols}<span className="middleSymbol">{middleSymbol}</span>{rightSikSymbols}{rightKikSymbols}</td>
       </tr>
     </>
   )
